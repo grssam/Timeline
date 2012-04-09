ea31ef8e7124c302219735c7e41a163a38ef541e
Girish Sharma <scrapmachines@gmail.com>
2012-04-09 15:53:31 +0530

    Add features

--- UI.js
+++ UI.js
@@ -28,6 +28,9 @@ function init() {
     rulerCtx.moveTo(t*pixelPerSecond, 0);
     rulerCtx.lineTo(t*pixelPerSecond, 25);
     rulerCtx.stroke();
+    rulerCtx.fillStyle = "#111";
+    rulerCtx.font = "12pt Arial";
+    rulerCtx.fillText("" + t + " s", t*pixelPerSecond + 1, 35);
     for (var ms = 1; ms < 10; ms++) {
       rulerCtx.lineWidth = 0.75;
       rulerCtx.strokeStyle = "#555555";
@@ -40,6 +43,8 @@ function init() {
   $("#networkCaption").click(openNetwork);
   $("#contentCaption").click(openContent);
   document.getElementById("timelineCanvas").addEventListener("DOMMouseScroll", mouseScrolled, true);
+  document.getElementById("overlayDiv").addEventListener("DOMMouseScroll", mouseScrolled, true);
+  document.getElementById("timelineCanvas").addEventListener("mousemove", onMouseMove, true);
 }
 
 function prepareTimeline(timeList, canvas, top, height, alpha) {
@@ -88,7 +93,7 @@ function expandNetworkTimeline(canvas) {
   var height = 190*(1 + animateTime/150);
   var alpha = (170 - animateTime)/150;
   prepareTimeline([timeline[0]], canvas, 5, height, alpha);
-  window.setTimeout(function() {expandNetworkTimeline(canvas);}, refreshRate);
+  window.mozRequestAnimationFrame(function() {expandNetworkTimeline(canvas);});
 }
 
 function collapseNetworkTimeline(canvas) {
@@ -103,7 +108,7 @@ function collapseNetworkTimeline(canvas) {
   var height = 40 + 50*(170 - animateTime)/150;
   var alpha = (170 - animateTime)/150;
   prepareTimeline(networkTimeline, canvas, 5, height, alpha);
-  window.setTimeout(function() {collapseNetworkTimeline(canvas);}, refreshRate);
+  window.mozRequestAnimationFrame(function() {collapseNetworkTimeline(canvas);});
 }
 
 function expandContentTimeline(canvas) {
@@ -118,7 +123,7 @@ function expandContentTimeline(canvas) {
   var height = 190*(1 + animateTime/150);
   var alpha = (170 - animateTime)/150;
   prepareTimeline([timeline[1]], canvas, 400 - height, height, alpha);
-  window.setTimeout(function() {expandContentTimeline(canvas);}, refreshRate);
+  window.mozRequestAnimationFrame(function() {expandContentTimeline(canvas);});
 }
 
 function collapseContentTimeline(canvas) {
@@ -133,7 +138,7 @@ function collapseContentTimeline(canvas) {
   var height = 40 + 50*(170 - animateTime)/150;
   var alpha = (170 - animateTime)/150;
   prepareTimeline(contentTimeline, canvas, 400 - height*contentTimeline.length, height, alpha);
-  window.setTimeout(function() {collapseContentTimeline(canvas);}, refreshRate);
+  window.mozRequestAnimationFrame(function() {collapseContentTimeline(canvas);});
 }
 
 function mouseScrolled(e) {
@@ -157,6 +162,16 @@ function mouseScrolled(e) {
 function openNetwork(event) {
   if (animateTime == 0)
     expandNetworkTimeline();
+  document.getElementById("timelineCanvas").title = "Scroll back to go back";
+  inNetwork = false;
+  inContent = false;
+  $("#overlayDiv").animate( {
+    opacity: 0,
+    top: '0px',
+    left: '0px',
+    'min-height': '0px',
+    'min-width': '0px',
+  }, 100);
   $("#contentDataTitle").animate( {
     opacity: 0,
     'min-height': '0px',
@@ -176,6 +191,8 @@ function openNetwork(event) {
 function closeNetwork(event) {
   if (animateTime == 0)
     collapseNetworkTimeline();
+  document.getElementById("timelineCanvas").title = "";
+  document.getElementById("overlayDiv").title = "Scroll to view the detailed event schedule";
   $("#networkDataTitle").animate( {
     opacity: 1,
     'min-height': '200px',
@@ -195,6 +212,16 @@ function closeNetwork(event) {
 function openContent(event) {
   if (animateTime == 0)
     expandContentTimeline();
+  inNetwork = false;
+  inContent = false;
+  document.getElementById("timelineCanvas").title = "Scroll back to go back";
+  $("#overlayDiv").animate( {
+    opacity: 0,
+    top: '0px',
+    left: '0px',
+    'min-height': '0px',
+    'min-width': '0px',
+  }, 100);
   $("#networkDataTitle").animate( {
     opacity: 0,
     'min-height': '0px',
@@ -214,6 +241,8 @@ function openContent(event) {
 function closeContent(event) {
   if (animateTime == 0)
     collapseContentTimeline();
+  document.getElementById("timelineCanvas").title = "";
+  document.getElementById("overlayDiv").title = "Scroll to view the detailed event schedule";
   $("#contentDataTitle").animate( {
     opacity: 1,
     'min-height': '200px',
@@ -228,4 +257,93 @@ function closeContent(event) {
     $("#contentCaption").removeClass("hidden");
     contentOpened = false;
   });
+}
+var lastId = -1;
+function highlightElement(x,y) {
+  var itemList = [[]];
+  y -= 50;
+  x -= 200;
+  var canvas = document.getElementById("timelineContainer");
+  if (networkOpened)
+    itemList = networkTimeline[Math.floor(y/(400/networkTimeline.length))%networkTimeline.length];
+  else if (contentOpened)
+    itemList = contentTimeline[Math.floor(y/(400/contentTimeline.length))%contentTimeline.length];
+  else
+    return;
+  var id = -1;
+  for (var index = 0; index < itemList.length; index++) {
+    var start = itemList[index][0];
+    var end = itemList[index][1];
+    if (!end)
+      end = start + 10*1000/pixelPerSecond;
+    if (x > (start/1000*pixelPerSecond - canvas.scrollLeft) && x < (end/1000*pixelPerSecond - canvas.scrollLeft)) {
+      id = index;
+      break;
+    }
+  }
+  if (lastId == id)
+    return;
+  if (id > -1) {
+    lastId = id;
+    var top = (networkOpened? Math.floor(y/(400/networkTimeline.length))*(400/networkTimeline.length)
+                            : Math.floor(y/(400/contentTimeline.length))*(400/contentTimeline.length));
+    var topOffset = networkOpened?Math.floor(y/(400/networkTimeline.length)):Math.floor(y/(400/contentOpened.length));
+    var width = !itemList[id][1]?10/1000*pixelPerSecond:(itemList[id][1]-itemList[id][0])/1000*pixelPerSecond;
+    var height = networkOpened? 400/networkTimeline.length: 400/contentTimeline.length;
+    height -= 10;
+    $("#overlayDiv").animate( {
+      opacity: 0.8,
+      'background-color': 'rgba(100,150,200,0.2)',
+      top: (50 + top + 2) + 'px',
+      left: (itemList[id][0]/1000*pixelPerSecond) + 'px',
+      'min-height': height + 'px',
+      'min-width': width + 'px',
+      'border-radius': '20px',
+      '-moz-box-shadow': '0px 0px 3px highlight',
+    }, 100, function() {
+      document.getElementById("overlayDiv").title = "Some Data";
+    });
+  }
+  else if (id == -1) {
+    lastId = -1;
+    $("#overlayDiv").animate( {
+      opacity: 0,
+      '-moz-box-shadow': 'none',
+    }, 100, function() {
+      document.getElementById("overlayDiv").title = "Scroll to view the detailed event schedule";
+    });
+  }
+}
+
+var inContent = false, inNetwork = false;
+function onMouseMove(e) {
+  if (contentOpened || networkOpened) {
+    highlightElement(e.clientX, e.clientY);
+    return;
+  }
+  var height = 400;
+  var y = e.clientY;
+  if (y <= height/2 && !inNetwork) {
+    inNetwork = true;
+    inContent = false;
+    $("#overlayDiv").animate( {
+      opacity: 0.2,
+      top: '50px',
+      left: '0px',
+      'min-height':Math.floor(height/2) + 'px',
+      'min-width': '2000px',
+      'border-radius': '0px',
+    }, 100);
+  } else if (y > height/2 && !inContent) {
+    inNetwork = false;
+    inContent = true;
+    $("#overlayDiv").animate( {
+      opacity: 0.2,
+      top: (Math.floor(height/2) + 50) + 'px',
+      left: '0px',
+      'min-height':Math.floor(height/2) + 'px',
+      'min-width': '2000px',
+      'border-radius': '0px',
+    }, 100);
+  }
 }
